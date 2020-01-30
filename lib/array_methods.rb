7d31bd41c0609c9144252ae29d2f15fe22808366
Beza Sirak <sirak.bezawit@gmail.com>
2020-01-30 12:06:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,11 @@
 def find_element_index(array, value_to_find)
  counter =0 
  while counter < array.length do
-   if (array[counter] == value_to_find)
+   if (array[counter] == value_to_find) do
      return counter
+   end
+   counter += 1
  end
- counter += 1
 end
 
 def find_max_value(array)
